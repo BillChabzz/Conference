7fdb638a307b6b14cd379b98fd5a54668ff25c4b
BillChabzz <chaby9.8@gmail.com>
2015-10-27 15:45:49 +0300

    added resource file and event detail activity

---
+++ app/src/main/java/bill/conf/fragz/conference/EventKeyDetail.java
@@ -0,0 +1,15 @@
+package bill.conf.fragz.conference;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bill on 10/27/15.
+ */
+public class EventKeyDetail extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
