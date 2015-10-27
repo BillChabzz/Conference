927ca8cdd6a50bd8bbadffa1928e3ae666efc695
BillChabzz <chaby9.8@gmail.com>
2015-10-27 15:47:36 +0300

    added resource file and event detail activity including correxponding resource files

--- app/src/main/java/bill/conf/fragz/conference/EventKeyDetail.java
+++ app/src/main/java/bill/conf/fragz/conference/EventKeyDetail.java
@@ -1,15 +1,29 @@
 package bill.conf.fragz.conference;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-/**
- * Created by bill on 10/27/15.
- */
 public class EventKeyDetail extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.event_key_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
