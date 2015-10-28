8dd124838589d5bee90b6e3a3162218ead7619dc
BillChabzz <chaby9.8@gmail.com>
2015-10-28 05:54:39 +0300

    set up gridview with resources

--- app/src/main/java/bill/conf/fragz/conference/EventKeyDetail.java
+++ app/src/main/java/bill/conf/fragz/conference/EventKeyDetail.java
@@ -1,16 +1,26 @@
 package bill.conf.fragz.conference;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class EventKeyDetail extends AppCompatActivity {
 
-    Integer[] imageIDs ={};
+    Integer[] imageIDs ={
+            R.drawable.c_guest,R.drawable.c_agenda,
+            R.drawable.c_info,R.drawable.c_twitter
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +37,52 @@ public class EventKeyDetail extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        GridView gridView= (GridView)findViewById(R.id.gridViewLegendary);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), "Pic " + (position +1) +" selected",Toast.LENGTH_SHORT).show();;
+            }
+        });
     }
 
+    public class ImageAdapter extends BaseAdapter{
+        private Context mContext;
+        public ImageAdapter(Context c){
+            mContext = c;
+        }
+
+        @Override
+        public int getCount() {
+            return imageIDs.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if(convertView == null){
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(180,180));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8,8,8,8);
+            }else{
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageResource(imageIDs[position]);
+            return imageView;
+        }
+    }
 
 }
